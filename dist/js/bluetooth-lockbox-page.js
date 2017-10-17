a55d4f3a09e1ef46e3b8fa0516ef8f4b92ec70b6
ereboz <ereboz8@gmail.com>
2017-10-17 13:08:47 -0700

    initial commit

---
+++ dist/js/bluetooth-lockbox-page.js
@@ -0,0 +1,35 @@
+// javaboxer 8-7-17
+// savlok-brochure
+
+// websites using the savlok-brochure have a bootstrap .container wrapper. We need to replace the parent container
+// with .container-fluid to accommodate full width for the brochure topic page.
+
+$(document).ready(function(){
+    
+    var loc = window.location.href.toLowerCase();
+    var pageWrapper = "";
+    
+    if (loc.indexOf('bluetoothlockbox') > -1) {
+        pageWrapper = $(".topic-page");
+        $(pageWrapper).parent().removeClass("container");
+        $(pageWrapper).parent().addClass("container-fluid");
+    }
+    
+});
+
+
+// make the primary boxes above the fold the same height base on which box's height is largest
+// we should add a window resize function as well
+
+// $(window).load(function() {
+//
+//     var highestBox = 0;
+//     var thisBox = $('div[class$="wrapper"] .box.primary');
+//     thisBox.each(function(){
+//         if($(this).height() > highestBox){
+//             highestBox = $(this).height();
+//         }
+//     });
+//     thisBox.height(highestBox);
+//
+// });
